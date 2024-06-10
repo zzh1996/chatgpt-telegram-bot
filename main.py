5c38e405eeb76c982fa4f0681ce38ef67a5a670f
zzh1996 <zhengzihan1996@gmail.com>
2024-06-09 19:33:30 -0700

    update triggers

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ ADMIN_ID = 71863318
 
 MODELS = [
     {'prefix': 'g$$', 'model': 'gemini-1.0-pro-latest', 'vision_model': 'gemini-pro-vision'},
+    {'prefix': 'g1$', 'model': 'gemini-1.0-pro-latest', 'vision_model': 'gemini-pro-vision'},
     {'prefix': 'g$', 'model': 'gemini-1.5-pro-latest'},
     {'prefix': 'gf$', 'model': 'gemini-1.5-flash-latest'},
 ]
