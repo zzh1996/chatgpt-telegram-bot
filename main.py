30ce28b642861dcec6abe45b95ed814cf5e528ff
zzh1996 <zhengzihan1996@gmail.com>
2024-06-20 11:42:20 -0700

    new model

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ signal.signal(signal.SIGUSR1, debug_signal_handler)
 ADMIN_ID = 71863318
 
 MODELS = [
-    {'prefix': 'cs$$$', 'model': 'claude-3-haiku-20240307'},
-    {'prefix': 'cs$$', 'model': 'claude-3-sonnet-20240229'},
-    {'prefix': 'cs$', 'model': 'claude-3-opus-20240229'},
+    {'prefix': 'cs$', 'model': 'claude-3-5-sonnet-20240620'},
+    {'prefix': 'c3s$', 'model': 'claude-3-opus-20240229'},
+    {'prefix': 'c3ss$', 'model': 'claude-3-sonnet-20240229'},
+    {'prefix': 'c3hs$', 'model': 'claude-3-haiku-20240307'},
 ]
 
 aclient = AsyncAnthropic(
