0dce06efd5523bd40ef28a94a310961bbed154ca
zzh1996 <zhengzihan1996@gmail.com>
2024-12-17 09:52:30 -0800

    change default model

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ signal.signal(signal.SIGUSR1, debug_signal_handler)
 ADMIN_ID = 71863318
 
 MODELS = [
-    {'prefix': 'g$', 'model': 'gemini-2.0-flash-exp'},
+    {'prefix': 'g$', 'model': 'gemini-exp-1206'},
     {'prefix': 'g15$', 'model': 'gemini-1.5-pro-latest'},
     {'prefix': 'gf$', 'model': 'gemini-1.5-flash-002'},
     {'prefix': 'g1$', 'model': 'gemini-1.0-pro-latest', 'vision_model': 'gemini-pro-vision'},
