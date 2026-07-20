f03f0e6ec672b0093976a4d38f17515dc94988e1
zzh1996 <zhengzihan1996@gmail.com>
2026-07-20 21:43:48 +0800

    new model

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ signal.signal(signal.SIGUSR1, debug_signal_handler)
 ADMIN_ID = 71863318
 
 MODELS = [
-    {'prefix': 'k$', 'model': 'kimi-k2.6', 'prompt_template': ''},
+    {'prefix': 'k$', 'model': 'kimi-k3', 'prompt_template': ''},
+    {'prefix': 'k26$', 'model': 'kimi-k2.6', 'prompt_template': ''},
     {'prefix': 'k25$', 'model': 'kimi-k2.5', 'prompt_template': ''},
     {'prefix': 'k2$', 'model': 'kimi-k2-0711-preview', 'prompt_template': ''},
     {'prefix': 'moonshot-v1-8k$', 'model': 'moonshot-v1-8k', 'prompt_template': ''},
