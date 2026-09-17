577f0d99b0d485a387ddf911a4ed83351fa80bf1
zzh1996 <zhengzihan1996@gmail.com>
2026-09-17 23:33:47 +0800

    new model

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ ADMIN_ID = 71863318
 
 MODELS = [
     {'prefix': 'dbe$', 'model': 'doubao-seed-evolving', 'prompt_template': ''},
-    {'prefix': 'db$', 'model': 'doubao-seed-2-1-pro-260628', 'prompt_template': ''},
+    {'prefix': 'db$', 'model': 'doubao-seed-2-1-pro-260915', 'prompt_template': ''},
+    {'prefix': 'db21$', 'model': 'doubao-seed-2-1-pro-260628', 'prompt_template': ''},
     {'prefix': 'dbt$', 'model': 'doubao-seed-2-1-turbo-260628', 'prompt_template': ''},
     {'prefix': 'db2$', 'model': 'doubao-seed-2-0-pro-260215', 'prompt_template': ''},
     {'prefix': 'db2l$', 'model': 'doubao-seed-2-0-lite-260215', 'prompt_template': ''},
