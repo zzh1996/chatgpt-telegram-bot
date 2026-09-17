5da54f9af5c25ade99eabab1a15d34a869ed4805
zzh1996 <zhengzihan1996@gmail.com>
2026-09-17 23:45:48 +0800

    new model

--- main.py
+++ main.py
@@ -24,7 +24,9 @@ signal.signal(signal.SIGUSR1, debug_signal_handler)
 ADMIN_ID = 71863318
 
 MODELS = [
-    {'prefix': 'z$', 'model': 'glm-5.2', 'prompt_template': ''},
+    {'prefix': 'z$', 'model': 'glm-5.3', 'prompt_template': ''},
+    {'prefix': 'zf$', 'model': 'glm-5.3-flash', 'prompt_template': ''},
+    {'prefix': 'z52$', 'model': 'glm-5.2', 'prompt_template': ''},
     {'prefix': 'z51$', 'model': 'glm-5.1', 'prompt_template': ''},
     {'prefix': 'z5$', 'model': 'glm-5', 'prompt_template': ''},
     {'prefix': 'z47$', 'model': 'glm-4.7', 'prompt_template': ''},
@@ -39,7 +41,7 @@ MODELS = [
     {'prefix': 'glm-4-flash$', 'model': 'glm-4-flash', 'prompt_template': ''},
 ]
 DEFAULT_MODEL = 'glm-4' # For compatibility with the old database format
-VISION_MODEL = 'glm-5v-turbo'
+VISION_MODEL = 'glm-5.3-flash'
 
 def get_prompt(model):
     if model == VISION_MODEL:
@@ -95,7 +97,7 @@ class ZhipuAI:
             'stream': True,
             'tools': [{'type': 'web_search', 'web_search': {'enable': False}}],
         }
-        if model in ['glm-4.5', 'glm-4.6', 'glm-4.7', 'glm-4.6v', 'glm-5', 'glm-5.1', 'glm-5v-turbo', 'glm-5.2']:
+        if model in ['glm-4.5', 'glm-4.6', 'glm-4.7', 'glm-4.6v', 'glm-5', 'glm-5.1', 'glm-5v-turbo', 'glm-5.2', 'glm-5.3', 'glm-5.3-flash']:
             payload['thinking'] = {
                 "type": "enabled",
             }
